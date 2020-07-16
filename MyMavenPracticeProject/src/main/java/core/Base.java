d257f0fa4a7f262455193a0ee12884a082b1ac48
MobinDawlatzai <mobinmobindawlatzai89@gmail.com>
2020-07-15 20:58:50 -0400

    initial commit

---
+++ MyMavenPracticeProject/src/main/java/core/Base.java
@@ -0,0 +1,16 @@
+package core;
+
+public class Base {
+	
+	
+	String x="i hate java programminhg";
+	
+	int i = 100;
+	public void clikOnButton() {
+		
+	}
+	public void verifiedButton() {
+		
+	}
+
+}
